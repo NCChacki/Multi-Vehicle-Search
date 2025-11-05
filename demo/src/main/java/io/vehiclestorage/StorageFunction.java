097a855fdcd1d864355b645583443db7577cd146
Chase Canning <142943245+NCChacki@users.noreply.github.com>
2025-11-04 17:22:03 -0700

    Nvm this is supposed to be fast

--- demo/src/main/java/io/vehiclestorage/StorageFunction.java
+++ demo/src/main/java/io/vehiclestorage/StorageFunction.java
@@ -1,15 +1,19 @@
 package io.vehiclestorage;
 
 
-import io.vehiclestorage.controller.StorageController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.function.Function;
 
 @Configuration
 public class StorageFunction {
 
     @Bean
-    public StorageController storageController() {
-        return new StorageController();
+    public Function<List<String>, List<String>> storagePlanner() {
+        return vehicles -> vehicles.stream()
+                .map(v -> "planned:" + v)
+                .toList();
     }
 }
